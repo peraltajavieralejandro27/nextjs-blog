31ae34280d9ed012a280f64afcf27349504929c2
Javier Alejandro Peralta <jperalta@appsolut.us>
2022-07-30 18:10:29 -0300

    Added archives.org data fetched with axios.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.scss';
-import {getSortedPostsData} from "../lib/posts";
+import archivesOrg from "../lib/archives";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+export async function getServerSideProps() {
+  const archives = await archivesOrg();
   return {
     props: {
-      allPostsData,
+      archives,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({archives}) {
   return (
     <Layout home>
       {/* Keep the existing code here */}
@@ -21,13 +21,13 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+          {archives.files.map((file,index) => (
+            <li className={utilStyles.listItem} key={index}>
+              <b>{file.name}</b>
+              <br/>
+              Source: {file.source}
+              <br/>
+              Mtime: {file.mtime}
             </li>
           ))}
         </ul>
